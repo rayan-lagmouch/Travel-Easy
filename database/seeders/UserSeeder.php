daeb3f9738e5c8525aaa8c604b6bf1ec52d804c8
mo121213132131 <hamoudishaaban16@gmail.com>
2025-02-21 12:52:35 +0100

    database seeder update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
         // Create a person for the admin
         $adminPerson = Person::create([
             'first_name' => 'shane',
-            'last_name' => 'User',
+            'last_name' => 'hamid',
             'date_of_birth' => '1990-01-01',
             'is_active' => true,
         ]);
@@ -35,7 +35,7 @@ class UserSeeder extends Seeder
         // Create a person for the employee
         $employeePerson = Person::create([
             'first_name' => 'kees',
-            'last_name' => 'chigga',
+            'last_name' => 'lagmouch',
             'date_of_birth' => '1990-01-01',
             'is_active' => true,
         ]);
@@ -51,8 +51,8 @@ class UserSeeder extends Seeder
 
         // Create a person for the regular user
         $userPerson = Person::create([
-            'first_name' => 'Regular',
-            'last_name' => 'monky',
+            'first_name' => 'riyan',
+            'last_name' => 'logmach',
             'date_of_birth' => '1990-01-01',
             'is_active' => true,
         ]);
